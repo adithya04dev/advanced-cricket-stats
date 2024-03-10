b278a8f19c0662427e7689105ec92d0a43897c2c
adithya04dev <127680046+adithya04dev@users.noreply.github.com>
2024-03-10 22:23:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,47 @@ from matchup import matchup
 from player_comp import player_comp
 from player_search import player_search
 from venue_stats import venue_search
+# import pandas as pd
+
+# # file_path = r"C:\Users\adith\Documents\Projects\python-projects\cric_metric_clone\json_csv\csv_files\t20_matches_new_format.csv"  # replace with your file path
+# # if st.button('Rerun'): st.experimental_rerun()
+# # file_path='https://csvfiles-cricmetric-clone.s3.ap-south-1.amazonaws.com/t20_matches_with_types.csv?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEBAaCmFwLXNvdXRoLTEiRzBFAiEA8hikMljYwKHQ0blrgTnhifb3itQ4zm1qHY70LfPbOf0CIAKBdM9zNCFKrawVWli8BXEJb9lhYum8RCntrpXvYPzxKuQCCBkQABoMODk3NzI2MDkxOTY1IgwH%2Fn8sFyOTfzrrbqoqwQJj2X%2BzZFdmico6upX%2BP5Nx5eYkKEwyMlN%2FcSFPF%2BObwadN4T%2Fc%2FH3G8J2OgT6sR%2B470%2FsJEHhRcITIJoL1p8Fk%2FGBL0e5rVEcG994OWPm%2FuLBpZJnOu2fcTH%2Bce81UKM%2Fuqjd2OEe4u%2BDBHutVV0eG9%2B3jPX955hj%2BRio71QS4MqlKDTLrMq%2B%2FKbwBHcMbxVpoHfxWxTQDhOP614coxaCxPRZLB9asAJW5yoqNTK5wyXeJm78pWimn7MzRstGpKPjPxwL7tZsNJ7xBrcghVPB9DQ3jLPMEc4rF5miyzVMxEP5cS4Gy0nrFJx17bWnafSrMQBWRJ4tWAVMLByW0SsFrcm3iLpj6NHs2TyvVwftBe21kclETOB%2F2ph1QdyqPpfPzUjLFUypMb1NhNSAk82Ms9BIVZhmBSqLoMKKcV6Or6IUwsbO3rwY6swKV3SIZeBAtOji2g3OiebVBxnLHAByWnBz2I%2Fa2vQpEFmNM2KnjIQ5cDs%2FKJr7Xuc3tjaI9UbNHNB06Awbc%2BCrXwo5cUx3EXVzVxDhNtQ1v1IKjWjnH4iOMaaG0AViQF4YL3cKWpO%2B1%2B%2FkMP1Y5di32QZa0DaKeQjrhiuEIosMpadq42pvKUEVCnsDhBZKO%2FJbk4Kz5a%2BI3UbDXCzYffeUjA%2FLlhivgoqZ6%2FE%2B8C4cXzwLdB9diugKEoPMsu6ELrY%2ByKUYQARgtM%2BFEqFFI12aQreJv4p6wMgUl2qLOIFgCWLDMpHWNzX8NyW%2BUhfIJlKx634D%2FstybLAgLNma%2F8JvLQ9LOXSl0sMq4Mkv33rhjDdCZo%2B7KmVCDQpitahB69u8I175MvBlfG6hwl6i0J9P9jG4P&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240310T160440Z&X-Amz-SignedHeaders=host&X-Amz-Expires=43200&X-Amz-Credential=ASIA5CBFFCK63EXHE5O7%2F20240310%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Signature=2015599ff0ae5b623a1ceb27d6c46eb616cd91694f0dfd9969a741938a3f9387'
+# # Load the dataframe
+# # df = pd.read_csv(file_path) 
+# from st_files_connection import FilesConnection
+
+# # Create connection object and retrieve file contents.
+# # Specify input format is a csv and to cache the result for 600 seconds.
+# conn = st.connection('s3', type=FilesConnection)
+# df = conn.read("csvfiles-cricmetric-clone/t20_matches_with_types.csv", input_format="csv")
+# import os
+import boto3
 import pandas as pd
 
-# file_path = r"C:\Users\adith\Documents\Projects\python-projects\cric_metric_clone\json_csv\csv_files\t20_matches_new_format.csv"  # replace with your file path
-# if st.button('Rerun'): st.experimental_rerun()
-# file_path='https://csvfiles-cricmetric-clone.s3.ap-south-1.amazonaws.com/t20_matches_with_types.csv?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEBAaCmFwLXNvdXRoLTEiRzBFAiEA8hikMljYwKHQ0blrgTnhifb3itQ4zm1qHY70LfPbOf0CIAKBdM9zNCFKrawVWli8BXEJb9lhYum8RCntrpXvYPzxKuQCCBkQABoMODk3NzI2MDkxOTY1IgwH%2Fn8sFyOTfzrrbqoqwQJj2X%2BzZFdmico6upX%2BP5Nx5eYkKEwyMlN%2FcSFPF%2BObwadN4T%2Fc%2FH3G8J2OgT6sR%2B470%2FsJEHhRcITIJoL1p8Fk%2FGBL0e5rVEcG994OWPm%2FuLBpZJnOu2fcTH%2Bce81UKM%2Fuqjd2OEe4u%2BDBHutVV0eG9%2B3jPX955hj%2BRio71QS4MqlKDTLrMq%2B%2FKbwBHcMbxVpoHfxWxTQDhOP614coxaCxPRZLB9asAJW5yoqNTK5wyXeJm78pWimn7MzRstGpKPjPxwL7tZsNJ7xBrcghVPB9DQ3jLPMEc4rF5miyzVMxEP5cS4Gy0nrFJx17bWnafSrMQBWRJ4tWAVMLByW0SsFrcm3iLpj6NHs2TyvVwftBe21kclETOB%2F2ph1QdyqPpfPzUjLFUypMb1NhNSAk82Ms9BIVZhmBSqLoMKKcV6Or6IUwsbO3rwY6swKV3SIZeBAtOji2g3OiebVBxnLHAByWnBz2I%2Fa2vQpEFmNM2KnjIQ5cDs%2FKJr7Xuc3tjaI9UbNHNB06Awbc%2BCrXwo5cUx3EXVzVxDhNtQ1v1IKjWjnH4iOMaaG0AViQF4YL3cKWpO%2B1%2B%2FkMP1Y5di32QZa0DaKeQjrhiuEIosMpadq42pvKUEVCnsDhBZKO%2FJbk4Kz5a%2BI3UbDXCzYffeUjA%2FLlhivgoqZ6%2FE%2B8C4cXzwLdB9diugKEoPMsu6ELrY%2ByKUYQARgtM%2BFEqFFI12aQreJv4p6wMgUl2qLOIFgCWLDMpHWNzX8NyW%2BUhfIJlKx634D%2FstybLAgLNma%2F8JvLQ9LOXSl0sMq4Mkv33rhjDdCZo%2B7KmVCDQpitahB69u8I175MvBlfG6hwl6i0J9P9jG4P&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240310T160440Z&X-Amz-SignedHeaders=host&X-Amz-Expires=43200&X-Amz-Credential=ASIA5CBFFCK63EXHE5O7%2F20240310%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Signature=2015599ff0ae5b623a1ceb27d6c46eb616cd91694f0dfd9969a741938a3f9387'
-# Load the dataframe
-# df = pd.read_csv(file_path) 
-from st_files_connection import FilesConnection
+# It's a good practice to set your AWS credentials as environment variables
+# os.environ['AWS_ACCESS_KEY_ID'] = 'YOUR_ACCESS_KEY'
+# os.environ['AWS_SECRET_ACCESS_KEY'] = 'YOUR_SECRET_KEY'
+# os.environ['AWS_DEFAULT_REGION'] = 'YOUR_REGION'
 
-# Create connection object and retrieve file contents.
-# Specify input format is a csv and to cache the result for 600 seconds.
-conn = st.connection('s3', type=FilesConnection)
-df = conn.read("csvfiles-cricmetric-clone/t20_matches_with_types.csv", input_format="csv")
+# However, for the sake of this example, I'll use the credentials you've provided
+# Please note that this is not a good practice and you should never hardcode your AWS credentials in your script
+session = boto3.Session(
+    aws_access_key_id='AKIA5CBFFCK6W6Z42WBB',
+    aws_secret_access_key='eMlYXpFmOIJsalO4+cCZ508I/OqVjoS/Iu1IAkxU',
+    region_name='ap-south-1'
+)
+
+# Create an S3 client
+s3 = session.client('s3')
+
+# Get the object from S3
+obj = s3.get_object(Bucket='csvfiles-cricmetric-clone', Key='t20_matches_with_types.csv')
+
+# Read the CSV file
+df = pd.read_csv(obj['Body'])
+
+# Now you can use the DataFrame as you like
+
 
 # Create a sidebar for navigation
 option = st.selectbox(
