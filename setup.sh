fe3af174d9858f40c319831f626f671839379d09
adithya04dev <127680046+adithya04dev@users.noreply.github.com>
2024-04-12 19:20:07 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,13 @@
-pip install -r requirements.txt
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"your_heroku@email_id.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
