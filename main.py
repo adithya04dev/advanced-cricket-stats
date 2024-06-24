fe1a0cf8003f7e3730a714c486e9d02411b980d6
adithya04dev <127680046+adithya04dev@users.noreply.github.com>
2024-06-24 19:33:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,10 @@ from player_search import player_search
 from venue_stats import venue_search
 import pandas as pd
 
-# # file_path = r"C:\Users\adith\Documents\Projects\python-projects\cric_metric_clone\json_csv\csv_files\t20_matches_new_format.csv"  # replace with your file path
-# # if st.button('Rerun'): st.experimental_rerun()
 
 # from dotenv import load_dotenv
 # load_dotenv()
 import os
-# df=pd.read_csv('https://csvfiles-cricmetric-clone.s3.ap-south-1.amazonaws.com/t20_matches_with_types.csv')
 # Create a sidebar for navigation
 option = st.selectbox(
     'Select an option',
