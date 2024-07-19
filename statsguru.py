08818dc88435f4edf6a36f4bbfbc4c45fe90d9bf
Adithya Balagoni <127680046+adithya04dev@users.noreply.github.com>
2024-07-19 08:46:08 +0530

    Update statsguru.py

--- statsguru.py
+++ statsguru.py
@@ -161,7 +161,7 @@ User Query :  {user_query}
 More info about correct refernces from userquery present in Datbase: {res_gem}. 
 
 Suggestions:
-1.t20, IT20  in match_type are mixed off there isnt any concrete difference between them
+1.T20, IT20  in match_type are mixed off there isnt any concrete difference between them
 2.Use \ as delimeter before ' is present in sql query
  """
 
