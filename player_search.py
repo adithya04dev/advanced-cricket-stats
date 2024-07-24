f6ac53a3e8e05996d830cbd808bec1338076768a
Adithya Balagoni <127680046+adithya04dev@users.noreply.github.com>
2024-07-24 18:37:11 +0530

    Update player_search.py

--- player_search.py
+++ player_search.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from big_query_engine_code import load_dropdown_values
 from   big_query_engine_code import calculate_stats
+import time
 def player_search():
     def get_user_inputs():
         col1, col2, col3 = st.columns(3)
@@ -108,6 +109,27 @@ def player_search():
     # Add a submit button
     if st.button('Submit',key='ps'):
         # Calculate stats
-        stats = calculate_stats( params)
-        # Display stats
-        st.write(stats)
+        st=time.time()
+        df2 = calculate_stats( params)
+
+        st.write(df2)
+        st.write(f" time taken for query {time.time()-st}")
+        df=df2.head(10)
+        df[df.columns[5]] = pd.to_numeric(df[df.columns[5]], errors='coerce')
+        df[df.columns[6]] = pd.to_numeric(df[df.columns[6]], errors='coerce')
+        fig, ax = plt.subplots()
+        scatter = ax.scatter(df[df.columns[5]], df[df.columns[6]])
+        
+        # Annotate each point with the batsman name
+        for i, row in df.iterrows():
+            ax.annotate(row[df.columns[0]], (row[df.columns[5]], row[df.columns[6]]), textcoords="offset points", xytext=(0,10), ha='center')
+        
+        ax.set_title(f'{df.columns[5]} vs {df.columns[6]}')
+        ax.set_xlabel(df.columns[5])
+        ax.set_ylabel(df.columns[6])
+        
+
+            
+        
+        # Display plot in Streamlit
+        st.pyplot(fig)
