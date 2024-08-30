f5be871f1b480d6fa4a6c0e2dd9058dce1e6a7a9
Adithya Balagoni <127680046+adithya04dev@users.noreply.github.com>
2024-08-30 15:54:48 +0530

    changed find_references to use tool calling agents

--- statsguru.py
+++ statsguru.py
@@ -26,18 +26,28 @@ import google.auth
 from google.oauth2 import service_account
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.tools import tool
+#import propmt template,agent executor,react agent
+from langchain.prompts import PromptTemplate
+from langchain.agents import AgentExecutor, create_react_agent
+
+from langchain_anthropic import ChatAnthropic
+from langchain_groq import ChatGroq
+import requests 
+import re
+import ast
+
+
+
 
 credentials_b64 = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
 credentials_bytes = base64.b64decode(credentials_b64)
 credentials_dict = json.loads(credentials_bytes)
 credentials = service_account.Credentials.from_service_account_info(credentials_dict)
 
-genai.configure(api_key=os.environ.get('GEMINI_API_KEY'))
-
-# llm=ChatTogether(model="meta-llama/Llama-3-70b-chat-hf")
 llm=ChatOpenAI(model='gpt-4o-mini')
-
-
 prompt = ChatPromptTemplate.from_messages(
     [
         (
@@ -50,50 +60,9 @@ prompt = ChatPromptTemplate.from_messages(
 )
 
 chain = prompt | llm
-
-
-
-vector_store_files=''
-for filename in os.listdir('./vector_store_files'):
-    with open(os.path.join('./vector_store_files', filename), 'r') as file:
-        filename=filename.replace('.txt','')
-        vector_store_files+=(filename+": column")
-        vector_store_files+='\n'
-        vector_store_files+=file.read()
-        vector_store_files+='\n'
 with open("./schema.txt", "r") as file:
     schema=file.read()
-prompt1="""
 
-Columns and its values in database: {vector_store_files}.
-Just try to store it as your context, i will be giving u some task based on this.
-
-"""
-query="""
-
-You are an intelligent agent designed to assist in generating SQL queries based on user inputs. 
-Users may provide   references to entity  names, such as player names, venues, or series names and some other columns in a cricket database.
-You need to identify those  references and replace those names to their exact names in the database in processed query.
-
-USE database TO accomplish the task.
-
-Here are the steps you should follow:
-1. Identify potential vague references in the user query that may correspond to any of the specified columns.
-2.Search from the below names that are in database.
-3.and return a json/dict with correct references in the database and also mention the column in which u found that refernce.
-json format:
-"references": [
-"original": ,
-"corrected": ,
-"column":]
-
-
-
-user query: {user_query}
-Dont return SQL query return json with correct references.
-
-
-"""
 prompt2="""  
 Imagine yourself as a sql assitant who writes sql code for calculating stats based on user query in a cricket database.
 I have a ball by ball bigquery database named bbbdata.ballsnew_2406 of cricket matches..
@@ -104,21 +73,16 @@ More info about correct refernces from userquery present in Database for where c
 THINK step by step..
 use COMMON TABLE EXPRESSIONS IF NEEDED.
 """ 
-f_prompt1=prompt1.format(vector_store_files=vector_store_files,schema=schema)
 
 history =None
 
 chat =None 
-# cache = caching.CachedContent.create(model="models/gemini-1.5-flash-001",display_name="database", system_instruction="You are a helpful assistant.",contents=[f_prompt1],ttl=timedelta(minutes=10),)
-# model = genai.GenerativeModel.from_cached_content(cached_content=cache) 
 n=0
-model = genai.GenerativeModel('gemini-1.5-flash') 
-chat_gemini = model.start_chat(history=[])
 
-response = chat_gemini.send_message(f_prompt1)
-def find_references(user_query):
+
+def find_references(user_query,model='gpt-4o-mini',stream=True):
     global history,chat
-    global n,chat_gemini,model
+    global n
     n=0
     history=ChatMessageHistory()
     chat = RunnableWithMessageHistory(
@@ -132,24 +96,276 @@ def find_references(user_query):
 
     res_gem=user_query
     res_gem+='\n'
-    f_user_query=query.format(user_query=user_query)
     ti=time.time()
-    try:
-        response = chat_gemini.send_message(f_user_query)
+    if model=='sonnet':
+        llm = ChatAnthropic(temperature=0, model_name="claude-3-sonnet-20240229")
+    elif model =='gpt-4o-mini':
+        llm = ChatOpenAI(model="gpt-4o-mini")
+    # elif model=='gemini':
+    #     llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+    else:
+        llm = ChatGroq(model='llama-3.1-70b-versatile')  
+  
+    @tool
+    def get_value_from_column(list1:str) -> str:
+        """
+        This function retrieves the specific value present in database column_name that is most similar to the search_value.
+        input: 2D list of lists  with the format shown below.
+        ([[column_name1,search_value1], [column_name2,search_value2], [column_name3,search_value3]]
+
+        """
+        def get_value_from_column1(list1:list) -> str:
+
+
+
+            column_name=list1[0]
+            search_value=list1[1]
+            if column_name not in ['venue', 'series_name',  'tournament_name',   'match_type',  'team_bat',  'team_bowl',  
+            'bowler_type', 'batter','bowler', 'bowler_kind','batter_hand']:
+                return """Can only  search for this columns ['venue', 'series_name',  'tournament_name',   'match_type',  'team_bat',  'team_bowl',  
+            'bowler_type', 'batter','bowler', 'bowler_kind','batter_hand']  in database"""
+            if column_name=='batter' or column_name=='bowler':
+                API_KEY = "3d1d6539895b4ec18c0adc1633fce5f3"
+                URL = "https://api.bing.microsoft.com/v7.0/search"
+
+                HEADERS = {"Ocp-Apim-Subscription-Key": API_KEY}
+                PARAMS = {
+                    "q": search_value + ' cricinfo ',
+                    "count": 10,
+                    "offset": 0,
+                    "mkt": "en-US",
+                    "safeSearch": "Moderate"
+                }
+
+                response = requests.get(URL, headers=HEADERS, params=PARAMS)
+                results = response.json()
+                flag = False
+
+                for result in results["webPages"]["value"]:
+                    url = result["url"]
+                    if 'https://www.espncricinfo.com/cricketers/' in url:
+                        flag = True
+                        break
+
+                if flag is False:
+                    return "Player not found Leave this column's value  empty."
+
+                parts = url.split("-")
+                id = parts[-1]
+                res = requests.get('http://core.espnuk.org/v2/sports/cricket/athletes/' + str(id))
+                name = res.json()['displayName']
+                return str(name)
+            else:
+
+            # Load FAISS index for specified column
+                embeddings = OpenAIEmbeddings(
+                    model='text-embedding-ada-002'
+                )
+                db = FAISS.load_local(fr"C:\Users\adith\Documents\Projects\python-projects\cric_metric_clone\vector_databases\{column_name}", embeddings, allow_dangerous_deserialization=True)
+
+                # Create document retriever and find most relevant document to player name
+                retriever = db.as_retriever(search_type='mmr', search_kwargs={'k': 5, 'lambda_mult': 1})
+                matched_value = retriever.get_relevant_documents(search_value)
+                res=[]
+                for i in matched_value:
+                    res.append(i.page_content)
+                return str(res)
+
+        # print(type(list1))
+        # print(list1)
+
+
+        list_str = re.search(r'\[.*\]', list1)
+        if list_str:
+            list_str = list_str.group()
+        else:
+            return "Invalid input format: No list found"
+        list1 = ast.literal_eval(list_str)
+        # list1 = eval(list_str)
+        # list1=eval(list1)
+
+        # print(type(list1))
+        # print(list1)
+        l=[]
+        for list2 in list1:
+            if len(list2)!=2:
+                print("list2",list2)
+                return "Inputs are not in correct format."
+                
+            res=get_value_from_column1(list2)
+            l.append(list2+[res])
+        print(l)
+        return l 
         
-        print(f"Total Tokens = {model.count_tokens(chat_gemini.history)}")
+    @tool
+    def add_2_numbers(a,b) :
+        """ 
+        This function adds 2 numbers.
+      """
+        return a+b
+        
 
+    tools = [get_value_from_column,add_2_numbers]
+    system_prompt=""" 
+   You are an helpful assistant.
+
+    """
+    user_prompt=f"""   
+    User Query: "{user_query}"
+    -
+    Finally you need to return the final answer with correct column names and values that are present in the database.
+    Thats why,you need  to preprocess these user queries by seraching in the database using the tool get_value_from_column
+    and then return the final answer.
+
+    
+
+
+    Some of the important columns,and its values in the database are:
+    - venue : many values are present in this column, so you may need to search in database using a function that is mentioned below.
+    - series_name : many values are present in this column, so you may need to search in database using a function that is mentioned below.
+    - tournament_name : many values are present in this column, so you may need to search in database using a function that is mentioned below.
+    - team_bat : many values are present in this column, so you may need to search in database using a function that is mentioned below.
+    - team_bowl : many values are present in this column, so you may need to search in database using a function that is mentioned below.
+    - batter : many values are present in this column, so you may need to search in database using a function that is mentioned below.
+    - bowler : many values are present in this column, so you may need to search in database using a function that is mentioned below.
+    - bowler_kind: ['pace bowler','spin bowler'] (you may need not search in database for this columns,values as they are only few)
+    - batter_hand :['Right Hand Batter','Left Hand Batter'] (you may need not search in database for this columns,values as they are only few)
+    - match_type : ['ODI','MDM','Test','IT20','T20','ODM'] (you may need not search in database for this columns,values as they are only few)
+
+    - bowler_type : ['RAP'(abbreviated for right arm pace),'OB'(abbreviated for off break),'LWS'(abbreviated for left wrist spin),
+    'LAP'(abbreviated for left arm pace),'LB'(abbreviated for left arm break),'SLA'(abbreviated for slow left arm)
+    ] (you may need not search in database for this columns,values as they are only few)
+
+    -another point is that tournament_name consists of  name of series,event..
+    but the seriesname consists of tournamanet_name+Season  of the series/event..
+      for example:tournament_namecan be :IPL ,series_name:IPL 2024 season: 2024
+    thus for query involving just the name of event you can use directly the tournament_name,if specifically
+    mentioned the event/tournament with a season/year you can use series_name.
+
+
+
+
+
+
+
+    """
+    MEMORY_KEY = "chat_history"
+    # prompt for tool calling agent
+    # prompt = ChatPromptTemplate.from_messages([
+    #     ("system", "you're a helpful assistant"), 
+    #     ("human", "{input}"), 
+    #     ("placeholder", "{agent_scratchpad}"),
+    # ])  
+    prompt= PromptTemplate.from_template(
+"""
+
+You are an intelligent agent designed to assist in generating SQL queries by processing  based on user query.
+
+
+You will be given a user query,in which users may provide vague references to entity names, such as batter,bowler,bowler_type,
+batter_hand,bowler_kind,match_type,team_bat,team_bowl,tournament_name,series_name,venue in a cricket database,
+but directly using those names/values of that column in SQL query may not work always,so you need to search for the exact name 
+of the  in the database using the tool get_value_from_column ( except for bowler_kind,batter_hand,match_type which have only f
+ew values and i have provided below  you with the list of those values).
+
+
+
+Thus you need  to preprocess these user queries by searching in the database using the tool get_value_from_column and 
+then return the final answer.
+
+
+Remember:
+-Even if in the query users mentioned other columns other than the ones mentioned above,you need to ignore them.    
+
+
+
+Important  info about databse
+    Some of the important columns,and its values in the database are:
+    - venue : many values are present in this column, so you may need to search in database using a function that is mentioned below.
+    - series_name : many values are present in this column, so you may need to search in database using a function that is mentioned below.
+    - tournament_name : many values are present in this column, so you may need to search in database using a function that is mentioned below.
+    - team_bat : many values are present in this column, so you may need to search in database using a function that is mentioned below.
+    - team_bowl : many values are present in this column, so you may need to search in database using a function that is mentioned below.
+    - batter : many values are present in this column, so you may need to search in database using a function that is mentioned below.
+    - bowler : many values are present in this column, so you may need to search in database using a function that is mentioned below.
+    - bowler_kind: ['pace bowler','spin bowler'] (you may need not search in database for this columns,values as they are only few)
+    - batter_hand :['Right Hand Batter','Left Hand Batter'] (you may need not search in database for this columns,values as they are only few)
+    - match_type : ['ODI','MDM','Test','IT20','T20','ODM'] (you may need not search in database for this columns,values as they are only few)
+
+    - bowler_type : ['RAP'(abbreviated for right arm pace),'OB'(abbreviated for off break),'LWS'(abbreviated for left wrist spin),
+    'LAP'(abbreviated for left arm pace),'LB'(abbreviated for left arm break),'SLA'(abbreviated for slow left arm)
+    ] (you may need not search in database for this columns,values as they are only few)
+
+    -another point is that tournament_name consists of  name of series,event..
+    but the seriesname consists of tournamanet_name+Season  of the series/event..
+      for example:tournament_namecan be :IPL ,series_name:IPL 2024 season: 2024
+    thus for query involving just the name of event you can use directly the tournament_name,if specifically
+    mentioned the event/tournament with a season/year you can use series_name.
+
+
+---------------------
+You have access to the following tools:
+
+{tools}
+---
+Tool names:
+{tool_names}
+---------------------------------------------------------------------------------------------------
+
+Use the following format:
+
+User Query: {input}
+
+Thought: Analyze the user query and determine what information needs to be retrieved from the database. 
+Consider which columns are relevant and what search terms to use.
+
+Action: get_value_from_column
+
+Action Input: [['column_name1', 'search_value1'],['column_name2', 'search_value2'], ...].
+
+Observation: Review the results returned by the action. 
+These are the most similar values found in the database for the given search terms.
+
+(Repeat the Thought/Action/Action Input/Observation steps N times, but dont repeat the same action again and again)
+
+Thought: Analyze the observations from all actions. 
+Determine which results are most relevant and accurate for answering the user query. 
+Consider any discrepancies or ambiguities in the data.
+
+Final Answer: Provide a concise response to the user query, including:
+-The relevant column names,appropriate values based on results of actions found for each column. from the database.
+But Dont include  sql query.
+Begin!
+
+User Query: {input}
+Thought: {agent_scratchpad}
+
+
+"""
+    )
+
+
+    chat_history = []
+    # prompt = hub.pull("hwchase17/react")
+    agent = create_react_agent(llm, tools, prompt)    
+    # agent = create_tool_calling_agent(llm, tools, prompt)
+
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+    try:
+        # result=agent_executor.invoke({"input": user_prompt})
+
+        result=agent_executor.invoke({"input": user_query})
     except Exception as e:
-        # cache = caching.CachedContent.create(model="models/gemini-1.5-pro-001",display_name="database", system_instruction="You are a helpful assistant.",contents=[f_prompt1],ttl=timedelta(minutes=10),)
-        # model = genai.GenerativeModel.from_cached_content(cached_content=cache) 
-        pass
-
-    res_gem+=response.text
-    print("Response: ",res_gem)
-    print("Time Taken for gemini-flash = ",time.time()-ti)
+        # print(e)
+        n=404
+        return {"response":str(e),"query":user_query,"f":None}
+    res_gem+=result['output']
+    # print("Response: ",res_gem)
+    print("Time Taken for gpt-4o-mini = ",time.time()-ti)
     
     res_gem+='\n\n'
     return {'response':res_gem,'query':user_query,'f':None}
+
 with open("sample_codes.txt","r") as f:
   sample_codes=f.read()
 
@@ -160,10 +376,11 @@ Schema and info about columns of  Database:
 Some of sample queries using this database are :{sample_codes}
 Write SQL QUERY for :
 User Query :  {user_query}
-More info about correct refernces from userquery present in Datbase: {res_gem}. use only this to filter dataset i.e in where of sql..
+More info about correct refernces from userquery present in Datbase: {res_gem}. 
+Use only this to filter dataset i.e in where of sql..
 
 Suggestions:
-1.Use \ as delimeter before ' is present in sql query.
+1.Use backslash as delimeter before ' is present in sql query.
 
 Think step by step and return sql query that can be executed.
  """
@@ -192,6 +409,8 @@ def coding(json_data):
     n+=1
     if n>4:
         remarks="Cannot be processed further. Simplify the Quey and try again."
+    if n==404:
+        remarks=res_gem
     if type(f)==pandas.core.frame.DataFrame:
        return [remarks,f]
     if f==None:
@@ -202,12 +421,17 @@ def coding(json_data):
     else:
        if type(f)!=pandas.core.frame.DataFrame:
           res=chat.invoke({"input": f"this error occurred{f}. Please try again and only provide rectified sql query"},{"configurable": {"session_id": "unused"}},)
-    critique=critique.format(user_query=user_query,sql_query=res.content,schema=schema)
+    # critique=critique.format(user_query=user_query,sql_query=res.content,schema=schema)
     # res=llm2.invoke(critique)
     print(f"time taken for sql query generation {time.time()-ti}") 
     result=res.content  
     sql_query = result.split('```')[-2].strip()
     sql_query=sql_query.replace('sql',' ')
+
+    print(f"sql query generated for {n}th iteration :",sql_query)
+
+
+
     return [sql_query,f]
 def router(inp):
     global n
@@ -252,6 +476,7 @@ workflow.add_edge('find_references', 'coding')
 workflow.add_edge('query_to_dataframe', 'coding')
 
 workflow.set_entry_point("find_references")
+
 
 
 def ask(question):
